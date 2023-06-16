c24a8f8f3830a13d6416c97fdc4674ca3686502b
MCNathaly8 <136032497+MCNathaly8@users.noreply.github.com>
2023-06-16 01:40:06 +0000

    implementacion de modulos

---
+++ programa.py
@@ -0,0 +1,10 @@
+from matematicas import sumar, restar, multiplicar, dividir
+from geometria import distancia
+
+print(sumar(2, 3))
+print(restar(4, 2))
+print (multiplicar(5,3))
+print(dividir(20,3))
+print("Distancia: ",distancia(1,2, 4,5))
+
+
